84028c1019bf518dc07764d0080d00975144f992
Jason Parham <bluemellophone@gmail.com>
2017-07-22 21:07:43 +0200

    Remove quiet

--- ibeis_cnn/models/classifier2.py
+++ ibeis_cnn/models/classifier2.py
@@ -105,8 +105,7 @@ class Classifier2Model(abstract_models.AbstractVectorModel):
             yb = [None] * len(Xb)
         arg_iter = list(zip(Xb, yb))
         result_list = ut.util_parallel.generate2(augment_parallel, arg_iter,
-                                                 ordered=True, verbose=False,
-                                                 quiet=True)
+                                                 ordered=True, verbose=False)
         result_list = list(result_list)
         X = [ result[0][0] for result in result_list ]
         X = np.array(X)
