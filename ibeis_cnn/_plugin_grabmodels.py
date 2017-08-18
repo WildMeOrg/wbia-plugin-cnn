889b6c14c30d5263196d5a5711fddc3e881fda83
Jason Parham <bluemellophone@gmail.com>
2017-08-18 17:27:01 -0400

    Correct model

--- ibeis_cnn/_plugin_grabmodels.py
+++ ibeis_cnn/_plugin_grabmodels.py
@@ -22,6 +22,7 @@ MODEL_URLS = {
     'classifier2_v3'                  : 'classifier2.29.pkl',
     'classifier2_candidacy'           : 'classifier2.candidacy.pkl',
     'classifier2_candidacy2'          : 'classifier2.candidacy2.pkl',
+    'classifier2_candidacy3'          : 'classifier2.candidacy3.pkl',
 
     'labeler_v1'                      : 'labeler.v1.pkl',
     'labeler_cheetah'                 : 'labeler.cheetah.pkl',
