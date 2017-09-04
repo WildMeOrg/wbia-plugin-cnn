5535e84fac32a41df31bb27d186aab38a7b134ef
Jason Parham <bluemellophone@gmail.com>
2017-09-04 12:13:55 -0400

    Small change

--- ibeis_cnn/models/aoi2.py
+++ ibeis_cnn/models/aoi2.py
@@ -72,7 +72,7 @@ def augment_wrapper(Xb, yb=None, wb=None):
                 mask[ytl: ybr, xtl: xbr] = 255
             X = np.dstack((X, mask))
             X[:, :, :3] = 0.0
-            X[:, :, 3] = 1.0
+            X[:, :, 3] = 255.0
             for channel in range(c + 1):
                 X_ = X[:, :, channel]
                 X_ = np.pad(X_, padding, 'reflect', reflect_type='even')
