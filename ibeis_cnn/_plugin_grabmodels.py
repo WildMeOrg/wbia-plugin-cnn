09d166c77324026d16f77a5fa0a13c1255339b59
Jason Parham <parham@wildme.org>
2018-03-06 10:03:29 -0800

    Add megan

--- ibeis_cnn/_plugin_grabmodels.py
+++ ibeis_cnn/_plugin_grabmodels.py
@@ -19,6 +19,7 @@ MODEL_URLS = {
     'classifier_cameratrap_megan2_v1'          : 'classifier.cameratrap.megan2.v1.pkl',
     'classifier_cameratrap_megan2_v2'          : 'classifier.cameratrap.megan2.v2.pkl',
     'classifier_cameratrap_megan2_v3'          : 'classifier.cameratrap.megan2.v3.pkl',
+    'classifier_cameratrap_megan2_v4'          : 'classifier.cameratrap.megan2.v4.pkl',
 
     'classifier_coco_zebra'                    : 'classifier.coco.zebra.pkl',
     'classifier_v3_zebra'                      : 'classifier.29.zebra.pkl',
