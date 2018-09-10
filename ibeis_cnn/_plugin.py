b09833183576ea36f323711e09a37cd6858e09a7
Jason Parham <parham@wildme.org>
2018-09-10 10:52:10 -0700

    Invert canvas

--- ibeis_cnn/_plugin.py
+++ ibeis_cnn/_plugin.py
@@ -674,7 +674,7 @@ def generate_species_background(ibs, chip_list, species=None, nInput=None):
         confidence_thresh = 0.2
         model = models.BackgroundModel(batch_size=batch_size, data_shape=data_shape)
         weights_path = grabmodels.ensure_model('background_jaguar', redownload=False)
-        canvas_key = 0
+        canvas_key = 1
     else:
         raise ValueError('species %r key does not have a trained model' % (species, ))
 
