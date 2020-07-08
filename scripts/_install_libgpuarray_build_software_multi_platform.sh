575440ed21a45137b2ce416860dcd662ae05efd9
Jason Parham <bluemellophone@gmail.com>
2020-07-08 00:26:35 -0700

    Small update:

--- scripts/_install_libgpuarray_build_software_multi_platform.sh
+++ scripts/_install_libgpuarray_build_software_multi_platform.sh
@@ -11,24 +11,22 @@ export WORKSPACE=$PWD
 export VIRTUAL_ENV="/opt/libgpuarray"
 
 # Checkout source, only if it isn't already laying around
-if [ ! -d ${WORKSPACE} ]; then
-    git clone https://github.com/Theano/libgpuarray.git ${WORKSPACE}/libgpuarray
-    cd ${WORKSPACE}/libgpuarray
-    git checkout ${VERSION}
-    rm -rf ${WORKSPACE}/libgpuarray/build
-    mkdir -p ${WORKSPACE}/libgpuarray/build
-    cd ${WORKSPACE}/libgpuarray/build
-    cmake \
-        -D CMAKE_BUILD_TYPE=RELEASE \
-        -D CMAKE_INSTALL_PREFIX=${VIRTUAL_ENV} \
-        ..
-    make -j9
-    sudo make install
-    sudo update_dyld_shared_cache
-    cd ..
-    python setup.py build_ext -L ${VIRTUAL_ENV}/lib -I ${VIRTUAL_ENV}/include
-    pip install -e .
-fi
+git clone https://github.com/Theano/libgpuarray.git ${WORKSPACE}/libgpuarray
+cd ${WORKSPACE}/libgpuarray
+git checkout ${VERSION}
+rm -rf ${WORKSPACE}/libgpuarray/build
+mkdir -p ${WORKSPACE}/libgpuarray/build
+cd ${WORKSPACE}/libgpuarray/build
+cmake \
+    -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=${VIRTUAL_ENV} \
+    ..
+make -j9
+sudo make install
+sudo update_dyld_shared_cache
+cd ..
+python setup.py build_ext -L ${VIRTUAL_ENV}/lib -I ${VIRTUAL_ENV}/include
+pip install -e .
 
 # Return to the workspace
 cd ${WORKSPACE}
