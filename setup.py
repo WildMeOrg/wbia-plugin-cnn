e8f38f2a75f9221563edba6fd3ea87b27263643e
karen chan <karen@karen-chan.com>
2020-07-09 18:27:52 +0100

    Add missing "import sys" in setup.py
    ```
        File "setup.py", line 127, in <module>
          install_requires = parse_requirements('requirements/runtime.txt')
        File "setup.py", line 120, in parse_requirements
          packages = list(gen_packages_items())
        File "setup.py", line 112, in gen_packages_items
          if not sys.version.startswith('3.4'):
      NameError: name 'sys' is not defined
      ----------------------------------------
    ERROR: Command errored out with exit status 1: /virtualenv/env3/bin/python3 /virtualenv/env3/lib/python3.6/site-packages/pip/_vendor/pep517/_in_process.py get_requires_for_build_wheel /tmp/tmp6a1gk6lq Check the logs for full command output.
    ```

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
+import sys
+
 import six
 from utool import util_setup
 from setuptools import setup
