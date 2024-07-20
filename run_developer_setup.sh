30821d3f52506546417c8dafcc9496c8f0f6bbf2
tsubramanian <tamilselvan@gmail.com>
2024-07-20 15:55:19 -0400

    Update run_developer_setup.sh

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -3,14 +3,14 @@
 ./clean.sh
 python3 setup.py clean
 
-pip3 install -r requirements.txt
+pip3 install --user -r requirements.txt
 
 python3 setup.py develop
 
-pip3 install -e .
+pip3 install --user -e .
 
-pip3 install -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
-pip3 install https://github.com/Lasagne/Lasagne/archive/master.zip
+pip3 install --user -r https://raw.githubusercontent.com/Lasagne/Lasagne/master/requirements.txt
+pip3 install --user https://github.com/Lasagne/Lasagne/archive/master.zip
 
 #cd Lasagne/
 #pip install -e .
